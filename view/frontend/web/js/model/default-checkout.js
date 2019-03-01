4e61ee20a9ad93b9e831da946f231dc1032c5c13
web expert <tothefuture1117@gmail.com>
2019-03-01 15:20:11 +0100

    Release 1.0.35

---
+++ view/frontend/web/js/model/default-checkout.js
@@ -0,0 +1,19 @@
+/**
+ * PostFinance Checkout Magento 2
+ *
+ * This Magento 2 extension enables to process payments with PostFinance Checkout (https://www.postfinance.ch/).
+ *
+ * @package PostFinanceCheckout_Payment
+ * @author customweb GmbH (http://www.customweb.com/)
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache Software License (ASL 2.0)
+ */
+define([], function(){
+	'use strict';
+	return {
+		storeAddresses: function(){},
+		
+		validateAddresses: function(){
+			return true;
+		}
+	};
+});
