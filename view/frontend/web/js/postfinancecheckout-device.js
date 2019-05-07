aed768de7bbb3f4414a1b6a4b09171d1d04eee39
web expert <tothefuture1117@gmail.com>
2019-05-07 15:23:59 +0200

    Release 1.0.43

--- view/frontend/web/js/postfinancecheckout-device.js
+++ view/frontend/web/js/postfinancecheckout-device.js
@@ -24,13 +24,13 @@ define([
 	return function(options){
 		var sessionIdentifier = $.mage.cookies.get('postfinancecheckout_device_id');
 		if (sessionIdentifier) {
-			loadScript(sessionIdentifier);
+			loadScript(options, sessionIdentifier);
 		} else {
 			$.getJSON(options.identifierUrl).fail(function (jqXHR) {
                 throw new Error(jqXHR);
             }).done(function(sessionIdentifier){
             	$.mage.cookies.set('postfinancecheckout_device_id', sessionIdentifier, { path: '/' });
-            	loadScript(sessionIdentifier);
+            	loadScript(options, sessionIdentifier);
             });
 		}
 		
