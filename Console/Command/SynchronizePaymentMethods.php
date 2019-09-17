ec474488f9a60147c7946000bc6cdb9f749a59c1
web expert <tothefuture1117@gmail.com>
2019-09-17 17:39:33 +0200

    Release 1.0.58

--- Console/Command/SynchronizePaymentMethods.php
+++ Console/Command/SynchronizePaymentMethods.php
@@ -31,7 +31,8 @@ class SynchronizePaymentMethods extends Command
      *
      * @param PaymentMethodConfigurationManagementInterface $paymentMethodConfigurationManagement
      */
-    public function __construct(PaymentMethodConfigurationManagementInterface $paymentMethodConfigurationManagement)
+    public function __construct(
+        PaymentMethodConfigurationManagementInterface $paymentMethodConfigurationManagement)
     {
         parent::__construct();
         $this->paymentMethodConfigurationManagement = $paymentMethodConfigurationManagement;
@@ -39,7 +40,8 @@ class SynchronizePaymentMethods extends Command
 
     protected function configure()
     {
-        $this->setName('postfinancecheckout:payment-method:synchronize')->setDescription('Synchronizes the PostFinance Checkout payment methods.');
+        $this->setName('postfinancecheckout:payment-method:synchronize')->setDescription(
+            'Synchronizes the PostFinance Checkout payment methods.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
